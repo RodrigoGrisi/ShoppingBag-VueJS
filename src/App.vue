7c2513f7ab885aeb8b01e237c27f0a088eced123
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-05-26 22:43:33 -0300

    Shopping bag - Prof Ivan Lourenço

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+
+<script setup>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+onMounted(() => {
+  store.dispatch('loadProducts')
+})
+</script>
+
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/basket">Shopping Bag </router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 1280px;
+  margin: 80px auto;
+}
+
+#nav {
+  padding: 16px 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(37, 37, 37);
+  color: white;
+  a {
+    color: white;
+    text-decoration: none;
+    margin-left: 20px;
+    &.router-link-exact-active {
+      color: #007bff;
+    }
+  }
+}
+</style>
